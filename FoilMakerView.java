1d7547cf2aed6574141b0659d8991d8d620c6830
brow1144 <brow1144@purdue.edu>
2016-10-24 19:00:35 -0400

    Add files via upload

---
+++ FoilMakerView.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ *
+ * CS180 Project03
+ *
+ * Foil Login Program.
+ *
+ * @author Kyle Brown, brow1144, G01
+ *
+ * @version 10/24/16
+ *
+ *
+ *
+ * CardLayout
+ *
+ * View = GUI
+ *
+ * Controller = Create a View object,
+ *              If button or something was pressed
+ *              It decides what to do with that information
+ *
+ */
+
+public class FoilMakerView extends JFrame {
+
+    public FoilMakerView() {
+
+        //gotologinscreen
+
+    }
+
+    public void goToLoginPanel() {
+
+    }
+
+    public void goToNewGamePanel() {
+        //remove pane from login panel
+        //add the new things
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
