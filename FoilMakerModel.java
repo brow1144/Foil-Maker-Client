36f1ff35c3c0733325cdff0623f41ad8ff0f2143
nfordyc <nfordyc@purdue.edu>
2016-10-25 19:35:03 -0400

    Update FoilMakerModel.java

--- FoilMakerModel.java
+++ FoilMakerModel.java
@@ -1,1 +1,23 @@
+/**
+ * Created by Nicholas on 10/24/2016.
+ */
+public class FoilMakerModel {
+    private String username;
+    private String userToken;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserToken() {
+        return userToken;
+    }
+
+    public void setUserToken(String userToken) {
+        this.userToken = userToken;
+    }
+}
