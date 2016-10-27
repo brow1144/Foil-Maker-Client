710533ceb99f0dd5ec62516b1bd24a7753201e58
nfordyc <nfordyc@purdue.edu>
2016-10-27 12:13:20 -0400

    Update FoilMakerModel.java

--- FoilMakerModel.java
+++ FoilMakerModel.java
@@ -2,42 +2,32 @@
  * Created by Nicholas on 10/24/2016.
  */
 public class FoilMakerModel {
-    private String username;
-    private String userToken;
-    private String gameToken;
+    private String username, userToken, gameToken;
 
     protected FoilMakerView view;
     protected FoilMakerController controller;
 
     public String getUsername() {
-
         return username;
     }
 
     public void setUsername(String username) {
-
         this.username = username;
     }
 
     public String getUserToken() {
-
         return userToken;
     }
 
     public void setUserToken(String userToken) {
-
         this.userToken = userToken;
-
     }
 
-     public void setGameToken(String gameToken) {
+    public void setGameToken(String gameToken) {
+        this.gameToken = gameToken;
+    }
 
-         this.gameToken = gameToken;
-
-     }
-
-     public String getGameToken() {
-
-         return gameToken;
-     }
+    public String getGameToken() {
+        return gameToken;
+    }
 }
